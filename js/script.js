0fd90e8390d27f93b25e8e40aa39910516ad5ce1
Sandeep Gangaram <sandeepgangaram@alumni.iitm.ac.in>
2022-10-14 15:51:49 +0530

    add dynamic nav open/close

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
 const yearElement = document.querySelector(".year");
 const currentYear = new Date().getFullYear();
 yearElement.textContent = currentYear;
+
+// Mobile Navigation
+
+const btnNavElement = document.querySelector(".btn-mobile-nav");
+const headerElement = document.querySelector(".header");
+const toggleNav = () => {
+  headerElement.classList.toggle("nav-open");
+};
+
+btnNavElement.addEventListener("click", toggleNav);
