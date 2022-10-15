9da07e2482bfbdede47936ab4074346c6b056fe5
Sandeep Gangaram <sandeepgangaram@alumni.iitm.ac.in>
2022-10-15 11:23:23 +0530

    add smooth scroll behavior for old safari browsers

--- js/script.js
+++ js/script.js
@@ -11,3 +11,38 @@ const toggleNav = () => {
 };
 
 btnNavElement.addEventListener("click", toggleNav);
+
+/*
+Smooth scroll  
+- for old browsers which don't support
+smooth scroll behavior css property
+ */
+
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+    const headerEl = document.querySelector("header");
+    if (href === "#") {
+      scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    }
+
+    if (href !== "#" && href.startsWith("#")) {
+      const scrollToEl = document.querySelector(href);
+      scrollToEl.scrollIntoView({
+        behavior: "smooth",
+      });
+    }
+    // headerEl.classList.toggle("nav-open"); //this alone leads to bug
+
+    // If nav-menu open. Just close it during any sort of click induced scroll
+    if (headerEl.classList.contains("nav-open")) {
+      headerEl.classList.remove("nav-open");
+    }
+  });
+});
